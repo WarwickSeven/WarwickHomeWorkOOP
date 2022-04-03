ed139d1b9192a79819cd680f80f26c35d2c51295
WarwickSeven <WarwickSeven@gmail.com>
2022-04-03 18:40:36 +0300

    OOP13

--- WarwickHomeWorkOOP/main.cpp
+++ WarwickHomeWorkOOP/main.cpp
@@ -1,14 +1,67 @@
-//
-//  main.cpp
-//  WarwickHomeWorkOOP
-//
-//  Created by Александр Тихонов on 03.04.2022.
-//
+//урок 1 задание 3
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+class Stack {
+    #define STACK_LENGHT 10
+    int arr[STACK_LENGHT] = {};
+    int volume = 0;
+public:
+    Stack() {
+        for (size_t i = 0; i < STACK_LENGHT; i++)
+        arr[i] = 0;
+    }
+    void reset()
+    {
+        while (volume > 0)
+        arr[volume--] = 0;
+    }
+    void print() {
+        cout << "( ";
+        for (size_t i = 0; i < volume; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << ")\n";
+    }
+    bool push(int a) {
+        if (volume < STACK_LENGHT) {
+            arr[volume++] = a;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    int pop() {
+        if (volume >= 0) {
+            return arr[volume--];
+        }
+            else {
+            cout << "Error" << endl;
+            return -1;
+            }
+    }
+    
+};
+
+int main()
+{
+    Stack stack;
+    stack.reset();
+    stack.print();
+
+    stack.push(3);
+    stack.push(7);
+    stack.push(5);
+    stack.print();
+
+    stack.pop();
+    stack.print();
+
+    stack.pop();
+    stack.pop();
+    stack.print();
+
     return 0;
 }
